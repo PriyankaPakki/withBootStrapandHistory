1f03c0b400f53f48a338b0b93ad4c87e26c33c01
Priyanka Pakki <priyanka19697@gmail.com>
2018-11-30 17:22:36 +0530

    Add files via upload

---
+++ dump.js
@@ -0,0 +1,21 @@
+// Code by Saleh Ibne Omar
+
+
+function calC(c) {
+
+    // form theke panel e jao then value bosao function calC ja value pabe ta 
+    // sei value + koro jno protita value add hoi
+    form.panel.value = form.panel.value + c;
+
+
+}
+
+
+function CE() {
+
+    // clear korar jonne function
+    // form theke panel e jao giye value bosao faka
+    form.panel.value = "";
+}
+
+alert("Basic Calculator, Upvote it if you liked it, Thanks!");
