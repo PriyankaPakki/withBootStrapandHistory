317ea3a1912ad79c244099c45271dada15cf6572
Priyanka Pakki <priyanka19697@gmail.com>
2018-12-09 22:00:23 +0530

    Add files via upload

--- calcy.js
+++ calcy.js
@@ -1,11 +1,46 @@
-function calC(c) {
+// A set of variables to define the calculator
 
-   screen.value = screen.value + c;
+const calculatorObject = {
+	screenValue:'0',
+	firstNumber:null,
+	flag:false,
+	secondNumber:null,
+	operator:null,
+	expression: "",
+	debug:true;
 
+};
 
+//Function that takes care when a digit is clicked
+
+function onClickNumber(number){
+
+	const {screenValue, flag, firstNumber} = calculatorObject;
+
+	//if first number exists and operator is not =
+	if(flag === true)
+	{
+		//then set the screen to the second number entered
+		calculatorObject.secondNumber = number;
+		calculatorObject.screenValue = calculatorObject.secondNumber;
+		//As we already got our second number we dont need it so
+		calculatorObject.flag = false;
+	}
+	else
+	{
+		//if its the first number and non-zero, we append to the value on screen
+		if(screenValue === '0')
+			calculatorObject.screenValue = number;
+		else
+			calculatorObject.screenValue = screenValue + number;
+	}
+
+	if(debug)
+		console.log(calculatorObject);
 }
 
+//Function to handle a decimal
 
-function CE() {
-    screen.value = "";
+function onClickDecimal(dot) {
+	if(calculatorObject.f
 }
